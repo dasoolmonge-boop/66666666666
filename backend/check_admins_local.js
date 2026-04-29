f89cfa76230b90d5172f46cb181d14c5e167c177
Dasoolmonge <dasoolmonge@example.com>
2026-04-29 16:36:27 +0700

    Fix admin database structure and notification logic

---
+++ backend/check_admins_local.js
@@ -0,0 +1,17 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+const dbPath = path.resolve(__dirname, './db/database.sqlite');
+const db = new sqlite3.Database(dbPath);
+
+console.log("--- ADMINS ---");
+db.all("SELECT * FROM admins", [], (err, rows) => {
+    if (err) console.error(err);
+    else console.table(rows);
+    
+    console.log("\n--- SUBSCRIBERS ---");
+    db.all("SELECT * FROM subscribers", [], (err, subRows) => {
+        if (err) console.error(err);
+        else console.table(subRows);
+        db.close();
+    });
+});
